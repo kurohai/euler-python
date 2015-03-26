880926106fa1ae7d6ebd25e35db598ff79d0f71f
kurohai <kurohai@gmail.com>
2015-03-26 16:13:35 -0500

    euler 1

---
+++ e1.py
@@ -0,0 +1,43 @@
+#!/bin/env python
+
+
+def mfive(num):
+    p = num * 5
+    if p < 1000:
+        return p
+    else:
+        return 0
+
+def mthree(num):
+    p = num * 3
+    if p < 1000:
+        return p
+    else:
+        return 0
+
+
+def docalc():
+    sum = 0
+    done = set()
+    for i in xrange(1000):
+        m = mfive(i)
+        done.add(m)
+        m = mthree(i)
+        done.add(m)
+
+    for d in done:
+        sum += d
+    print sum
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    docalc()
