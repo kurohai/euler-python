30ddd24532cd7e8c4e7a3823e066ea91223e3fa3
kurohai <kurohai@gmail.com>
2015-03-27 07:49:02 -0500

    euler 7

---
+++ e7.py
@@ -0,0 +1,30 @@
+#!/bin/env python
+
+
+def IsPrime(num):
+    if num == 1:
+        return False
+    counter = 2
+    while counter < num:
+        if not num % counter:
+            return False
+        counter += 1
+    return True
+
+
+def Main(count):
+    done = False
+    i = 0
+    primes = list()
+    while not done:
+        i += 1
+        if IsPrime(i):
+            primes.append(i)
+            print '{0}\t{1}'.format(len(primes), i)
+        if len(primes) == count:
+            done = True
+    print primes[-1]
+
+
+if __name__ == '__main__':
+    Main(10001)
