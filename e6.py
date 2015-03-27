015eb9a12f66d96b7e72ae52e2e72472181d48f5
kurohai <kurohai@gmail.com>
2015-03-27 07:34:13 -0500

    euler 6

---
+++ e6.py
@@ -0,0 +1,28 @@
+#!/bin/env python
+
+
+def square(num):
+    return num * num
+
+def sum(num1, num2):
+    return num1 + num2
+
+squares = 0
+sums = 0
+for i in xrange(101):
+    squares += square(i)
+    sums += i
+
+def Main():
+    squares = 0
+    sums = 0
+    for i in xrange(101):
+        squares += square(i)
+        sums += i
+    sumsq = square(sums)
+    print sumsq - squares
+
+
+
+if __name__ == '__main__':
+    Main()
