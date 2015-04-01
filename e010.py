8e9d33460828fb0fab03b1e0d5a35d46aad9977e
kurohai <kurohai@gmail.com>
2015-03-31 20:28:07 -0500

    euler 10

---
+++ e010.py
@@ -0,0 +1,24 @@
+#!/bin/env python
+
+
+def SoE(limit):
+    a = [True] * limit
+    a[0] = a[1] = False
+    for (i, isprime) in enumerate(a):
+        if isprime:
+            yield i
+            for n in range(i*i, limit, i):
+                a[n] = False
+
+
+def Main(limit):
+    sum = 0
+    for i in SoE(limit):
+        sum += i
+    return sum
+
+
+if __name__ == '__main__':
+    limit = 2000000
+    answer = Main(limit)
+    print answer
